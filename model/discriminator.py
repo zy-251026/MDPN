22bd652046bfb59761162edd4715e77d838bd6c4
zy <63138940+zy-251026@users.noreply.github.com>
2024-03-22 20:21:35 +0900

    Update discriminator.py

--- model/discriminator.py
+++ model/discriminator.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from morph_layers2D_torch import *
+from model.morph_layers2D_torch import *
 
 class Discriminator(nn.Module):
     
